318d398e289f17971f8fd493ca2f7815b6b27042
pkbdmp <pkbsdmp@gmail.com>
2019-08-31 11:29:22 +0530

    updating packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pythoneasy',
-    version='1.0',
+    version='1.1',
     url='https://github.com/pupattan/pythoneasy',
     license='MIT',
     author='pupattan',
